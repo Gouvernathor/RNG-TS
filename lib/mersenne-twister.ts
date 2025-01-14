57a3911730c129c76e59e60b152755e925297675
Gouvernathor <44340603+Gouvernathor@users.noreply.github.com>
2025-01-14 13:52:06 +0100

    Use magic constants

--- lib/mersenne-twister.ts
+++ lib/mersenne-twister.ts
@@ -1,9 +1,13 @@
 import AbstractRNG from "../abstract";
 import hashString from "../hashString";
 
+// magics
+const _624 = 624;
+const _397 = 397;
+
 export default class MersenneTwister extends AbstractRNG {
     #index!: number;
-    readonly #state: number[] = Array(624);
+    readonly #state: number[] = Array(_624);
 
     constructor(seed?: number|string) {
         super();
@@ -23,7 +27,7 @@ export default class MersenneTwister extends AbstractRNG {
             Math.floor(Math.random() * 0xffffffff) | 0 :
             seed;
 
-        for (let i = 1; i < 624; i++) {
+        for (let i = 1; i < _624; i++) {
             state[i] = state[i-1] ^ (state[i-1] >>> 30);
             state[i] = 0x6c078965 * state[i] + i; // 1812433253
             state[i] = state[i] & ((state[i] << 32) - 1);
@@ -32,13 +36,13 @@ export default class MersenneTwister extends AbstractRNG {
 
     #generateNumbers() {
         const state = this.#state;
-        for (let i = 0; i < 624; i++) {
+        for (let i = 0; i < _624; i++) {
             // Bit 31 (32nd bit) of state[i]
             let y = (state[i] & 0x80000000);
             // Bits 0-30 (first 31 bits) of state[...]
-            y += (state[(i+1) % 624] & 0x7fffffff);
+            y += (state[(i+1) % _624] & 0x7fffffff);
             // The new pseudo random number
-            state[i] = state[(i + 397) % 624] ^ (y >>> 1);
+            state[i] = state[(i + _397) % _624] ^ (y >>> 1);
             if (y % 2 !== 0) { // y is odd
                 state[i] ^= 0x9908b0df; // 2567483615
             }
@@ -57,7 +61,7 @@ export default class MersenneTwister extends AbstractRNG {
         y ^= (y << 15) & 0xefc60000; // 4022730752
         y ^= y >>> 18;
 
-        this.#index = (this.#index + 1) % 624;
+        this.#index = (this.#index + 1) % _624;
 
         return (y >>> 0) * (1.0 / 4294967296.0);
     }
