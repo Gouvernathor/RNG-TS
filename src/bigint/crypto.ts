7bbb881b0f744c824676b091c0fa5df2958ea634
Gouvernathor <44340603+Gouvernathor@users.noreply.github.com>
2026-04-20 01:26:47 +0200

    crypto implem

---
+++ src/bigint/crypto.ts
@@ -0,0 +1,27 @@
+import { Fraction } from "@gouvernathor/fraction.ts";
+import AbstractBigIntRNG from "./abstract";
+
+/**
+ * Uses the web-standard Crypto object to generate random values.
+ * NOT SEEDABLE NOR DETERMINISTIC.
+ */
+export default class CryptoBigIntRNG extends AbstractBigIntRNG {
+    override random(resolution = this.resolution): Fraction {
+        const requestedNBits = resolution.toString(16).length * 4;
+
+        const length = Math.ceil(requestedNBits / 64);
+
+        const array = new BigUint64Array(length);
+        crypto.getRandomValues(array);
+
+        const numerator = array.reduce((prev, curr) => (prev << 64n) + curr);
+
+        const denominator = 2n ** (64n * BigInt(length));
+
+        return Fraction.fromPair(numerator, denominator);
+    }
+
+    override set seed(_seed: string | number | bigint | undefined) {
+        console.error("Seeding is not supported with this generator.");
+    }
+}
