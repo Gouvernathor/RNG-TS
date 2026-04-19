d66247eee63bcb0cac05936b18a014a76d851066
Gouvernathor <44340603+Gouvernathor@users.noreply.github.com>
2026-04-20 01:29:32 +0200

    expose in the main export

--- src/bigint/RNG.ts
+++ src/bigint/RNG.ts
@@ -2,12 +2,14 @@ import AbstractBigIntRNG from "./abstract.js";
 import Lehmer128BigIntRNG from "./lehmer.js";
 import DefaultBigIntRNG from "./default.js";
 import WellBigIntRNG from "./well.js";
+import CryptoBigIntRNG from "./crypto.js";
 
-export { Lehmer128BigIntRNG, DefaultBigIntRNG, WellBigIntRNG };
+export { Lehmer128BigIntRNG, DefaultBigIntRNG, WellBigIntRNG, CryptoBigIntRNG };
 export type { AbstractBigIntRNG as AnyBigIntRNG };
 
 export default class RNG extends WellBigIntRNG {
     static L128 = Lehmer128BigIntRNG;
     static D = DefaultBigIntRNG;
-    static W = WellBigIntRNG;
+    static Well = WellBigIntRNG;
+    static Crypto = CryptoBigIntRNG;
 }
