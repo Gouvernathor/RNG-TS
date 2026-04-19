947e7ab3972c1581c0d0dd09d759461d041bda8a
Gouvernathor <44340603+Gouvernathor@users.noreply.github.com>
2026-04-19 23:00:44 +0200

    remove todo

--- src/bigint/abstract.ts
+++ src/bigint/abstract.ts
@@ -1,31 +1,5 @@
 import { Fraction, FractionAble } from "@gouvernathor/fraction.ts";
 
-/**
- * TODO
- * how to make this work:
- * instead of using one reseed to generate a single value of x significant bits,
- * use as many reseeds as necessary to generate the required number of bits
- * for the requested (minimal) resolution.
- * This breaks the guarantee of a constant number of reseeds for each method call,
- * though it is now proportional to the resolution (or to the span for randrange).
- * It should remain proportional to the number of values for choice(s).
- *
- * Use a resolution setter ?
- * and default random()'s parameter to it ?
- * Defaults to 0, and that throws an error in random()
- * saying that it must be strictly positive.
- * Do that at the end, removing the resolution parameter to all the other methods,
- * and for those that call random() several times and also pass a resolution through,
- * document what the resolution is relative to.
- *
- * OR have randRange(one parameter) be the root function ??
- * no, because if the requested number and the reseed max value are coprimes,
- * it can be hard and slow to reconcile them.
- * (and both not generate some values more frequently than others,
- * and also not generate bigger numbers than expected.)
- * Whereas it makes no sense to complain that your fraction is more precise than you asked,
- * as long as its value is in the expected range.
- */
 export default abstract class AbstractBigIntRNG {
     /**
      * This is the default resolution value passed to the random method.
